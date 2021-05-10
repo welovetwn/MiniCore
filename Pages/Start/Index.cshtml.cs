e1272e55a5996705683229da3efc547280fef837
Johan Filipsson <johan@domitor.se>
2021-05-10 17:08:53 +0200

    First Commit

---
+++ Pages/Start/Index.cshtml.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Intwenty.DataClient;
+using Intwenty.DataClient.Reflection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiniCore.Pages
+{
+
+    public class IndexModel : PageModel
+    {
+
+
+
+        public IndexModel()
+        {
+           
+        }
+
+        public void OnGet()
+        {
+           var dbclient = new Connection(DBMS.SQLite, "Data Source=wwwroot/sqlite/MiniCore.db");
+           if (!dbclient.TableExists("Product"))
+           {
+               dbclient.CreateTable<Product>();
+               dbclient.CreateTable<ProductDependency>();
+
+               var id = dbclient.InsertEntity(new Product{ Name="Mini Core", Dependencies=new List<ProductDependency>() });
+               dbclient.InsertEntity(new ProductDependency(){ Name="AspNetCore", Version="3.1", Purpose="The server side platform, razor, routing etc", ProductId=id });
+               dbclient.InsertEntity(new ProductDependency(){ Name="JQuery", Version="3.6.0", Purpose="To simplify client java scripts", ProductId=id });
+               dbclient.InsertEntity(new ProductDependency(){ Name="Vue.Js", Version="3", Purpose="Data binding, reactivity etc... client side", ProductId=id });
+               dbclient.InsertEntity(new ProductDependency(){ Name="Bootstrap", Version="4", Purpose="A fast way to a nice looking UI", ProductId=id });
+               dbclient.InsertEntity(new ProductDependency(){ Name="Intwenty Data Client", Version="1.1.0", Purpose="Quick database access to common db engines", ProductId=id });
+               dbclient.InsertEntity(new ProductDependency(){ Name="SqlLite", Version="?", Purpose="A simple file db just for demo...", ProductId=id });
+
+           }
+
+           dbclient.Close();
+
+        }
+
+        public IActionResult OnGetList()
+        {
+             var dbclient = new Connection(DBMS.SQLite, "Data Source=wwwroot/sqlite/MiniCore.db");
+             var p = dbclient.GetEntities<Product>().FirstOrDefault();
+             if (p.Id>0)
+             {
+                 var deps = dbclient.GetEntities<ProductDependency>(string.Format("select * from ProductDependency where Productid={0}",p.Id));
+                 p.Dependencies=deps;
+             }
+             dbclient.Close();
+            return new JsonResult(p);
+        }
+
+      
+
+       
+    }
+
+    [DbTablePrimaryKey("Id")]
+    [DbTableName("Product")]
+    public class Product
+    {
+        [AutoIncrement]
+        public int Id {get; set;}
+
+        public string Name {get; set;}
+
+        [Ignore]
+        public List<ProductDependency> Dependencies {get; set;}
+
+    }
+
+    [DbTablePrimaryKey("Id")]
+    [DbTableName("ProductDependency")]
+    public class ProductDependency
+    {
+        [AutoIncrement]
+        public int Id {get; set;}
+
+        public int ProductId {get; set;}
+
+        public string Name {get; set;}
+        public string Version {get; set;}
+        public string Purpose {get; set;}
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+public class Program
+{
+    public static void Main(string[] args) =>
+
+        Host.CreateDefaultBuilder(args)    
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+            //webBuilder.UseKestrel();
+            webBuilder.ConfigureServices((buildercontext, services) =>
+            {
+                services.AddRazorPages();
+
+            }).Configure(app =>
+            {
+                app.UseStaticFiles();
+                app.UseRouting();
+                app.UseEndpoints(route =>
+                {
+                    route.MapRazorPages();
+                    //route.MapGet("/", context => context.Response.WriteAsync("Hello world"));
+                });
+            });
+        })
+        .Build().Run();
+}
